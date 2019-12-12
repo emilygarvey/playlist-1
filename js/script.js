08bd419b378a96f7e87afa2ead9e142431a7afec
Emily Emily <emilygarvey@gmail.com>
2019-12-12 22:23:16 +0000

    day 3

--- js/script.js
+++ js/script.js
@@ -1,18 +1,49 @@
-let songNames = ["Happy Birthday", "Jingle Bells", "Silent Night"];
+//let songNames = ["Happy Birthday", "Jingle Bells", "Silent Night"];
 let imageLinks = [];
 
 let newSong = '';
 
+/*
 $("button").click(function() {
     newSong = $("#songInput").val();
     songNames.push(newSong);
-    console.log(songNames);
+    $("#songs").empty();
+    for (let song of songNames) {
+        $("#songs").append(`<p>${song}</p>`);
+    }
 });
+*/
 
-for (let song of songNames) {
-    $("#songs").append(`<p>${song}</p>`);
-}
+//for (let song of songNames) {
+//    $("#songs").append(`<p>${song}</p>`);
+//}
 
 for (let image of imageLinks) {
        
 }
+
+let song1 = {
+    song: "Happy Birthday"
+}
+let song2 = {
+    song: "Jingle Bells"
+}
+let song3 = {
+    song: "Silent Night"
+}
+
+let newItem = {
+    song: newSong,
+}
+
+let playlist = [song1, song2, song3];
+console.log(playlist);
+
+$("button").click(function() {
+    newSong = $("#songInput").val();
+    playlist.push(newItem);
+    $("#songs").empty();
+    for (let item of playlist) {
+        $("#songs").append(`<p>${item.song}</p>`);
+    }
+});
